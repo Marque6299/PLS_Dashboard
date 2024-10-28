93df47529b29de5cae45b05c0d616f8388e6e007
Marque6299 <165474979+Marque6299@users.noreply.github.com>
2024-10-28 11:00:23 -0700

    redo

--- VBS Workshop/script.js
+++ VBS Workshop/script.js
@@ -35,7 +35,7 @@ function updateChart(data) {
     if (chart) chart.destroy();
 
     chart = new Chart(ctx,{
-        type: 'pie',
+        type: 'line',
         data: {
             labels: labels,
             datasets: [{
